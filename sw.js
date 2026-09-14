667428014083b67ec5bcbd37da41f57baf912766
lucasabraham1996-cmd <lucasabraham1996@gmail.com>
2026-09-14 14:53:54 -0300

    Forzar actualización móvil tras corregir superposiciones

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rutina-entreno-v8';
+const CACHE='rutina-entreno-v9';
 const CORE=['./','./index.html','./manifest.webmanifest','./ios.css','./premium.css','./customize.js','./firebase-sync.js','./alternatives.css','./alternatives.js','./bodymap.js'];
 
 self.addEventListener('install',event=>{
