aa810a673f8baa6bacab69c5cd98fcd8cae37f0a
lucasabraham1996-cmd <lucasabraham1996@gmail.com>
2026-09-14 09:08:36 -0300

    Actualizar caché para Firebase

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='rutina-entreno-v4';
-const CORE=['./','./index.html','./manifest.webmanifest','./ios.css','./customize.js'];
+const CACHE='rutina-entreno-v5';
+const CORE=['./','./index.html','./manifest.webmanifest','./ios.css','./customize.js','./firebase-sync.js'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
